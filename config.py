f4afb1018c9d4f8fa0a283ce2028ee9bccb055af
pedro <pedro9@github.com>
2016-02-14 00:29:21 -0200

    solving prob not running locally

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = " l\x96\xb0\xe7\xfe\x8dRa\xad`\x13\x9ci|\x16\x9fG\xcc\xe0\t-\xaf\xa4"
+    # shortened for readability
+    SECRET_KEY = ' l\x96\xb0\xe7\xfe\x8dRa\xad`\x13\x9ci|\x16\x9fG\xcc\xe0\t-\xaf\xa4'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
@@ -13,4 +14,4 @@ class DevelopmentConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-	    DEBUG = False
+    DEBUG = False
