198569167a674fb071da8142fbf88421c3192bef
pedro <pedro9@github.com>
2016-02-14 13:19:36 -0200

    part 14 - Update to equal original git rep

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class BaseConfig(object):
     # shortened for readability
     SECRET_KEY = ' l\x96\xb0\xe7\xfe\x8dRa\xad`\x13\x9ci|\x16\x9fG\xcc\xe0\t-\xaf\xa4'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # print SQLALCHEMY_DATABASE_URI
 
 
 class DevelopmentConfig(BaseConfig):
